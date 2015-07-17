a7e9bf14ad764e161279ec6fc4866ecd9ba0dc60
FunkyFlier <mike@kaslingaircraft.com>
2015-07-17 13:03:57 -0500

    Adjust feedback gains

--- Inertial.h
+++ Inertial.h
@@ -8,11 +8,11 @@
 #define RC_CONST_BARO 1/(2.0 * 3.14 * FC_BARO)
 
 #define K_P_GPS 0.1
-#define K_V_GPS 0.2
-#define K_B_GPS 0.03
+#define K_V_GPS 0.22
+#define K_B_GPS 0.003
 #define K_P_BARO 0.07
-#define K_V_BARO 0.1
-#define K_B_BARO 0.01
+#define K_V_BARO 0.12
+#define K_B_BARO 0.003
 
 void GetInertial();
 void Predict(float);
